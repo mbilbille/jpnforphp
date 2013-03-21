95bd56cf36ca6e2ea95a189a8aa13e3e52b47364
Matthieu Bilbille <matthieu.bilbille@gmail.com>
2013-03-21 15:11:25 +0100

    Move default mappings into Romanization

--- src/JpnForPhp/Transliterator/Romanization.php
+++ src/JpnForPhp/Transliterator/Romanization.php
@@ -16,24 +16,103 @@ namespace JpnForPhp\Transliterator;
  */
 abstract class Romanization implements RomanizationInterface
 {
-    // Characters to espace before the romanization process.
-    const PREG_PATTERN_ESCAPE_CHAR =  '/([a-z]+)/';
+    /**
+     * @var string Regex pattern to escape latin characters.
+     */
+    const PREG_PATTERN_ESCAPE_CHAR = '/([a-z]+)/';
 
+    /**
+     * @var array Store latin characters which are escaped.
+     */
     public $latinCharacters = array();
+
+
+    /**
+     * @var array Map hiragana characters (or combinaison of characters) to
+     * their equivalent in latin alphabet.
+     *
+     * This mapping must be defined in Romanization' subclasses.
+     */
+    protected $mapHiragana = array();
+
+    /**
+     * @var array Map katakana characters (or combinaison of characters) to
+     * their equivalent in latin alphabet.
+     *
+     * Here is only extended katakana, mapping for default katakana must be defined in Romanization' subclasses.
+     *
+     * @see http://en.wikipedia.org/wiki/Transcription_into_Japanese#Extended_katakana
+     */
+    protected $mapKatakana = array(
+        'イィ' => 'yi', 'イェ' => 'ye',
+        'ウァ' => 'wa', 'ウィ' => 'wi', 'ウゥ' => 'wu', 'ウェ' => 'we', 'ウォ' => 'wo',
+        'ウュ' => 'wya',
+        'ヴァ' => 'va', 'ヴィ' => 'vi', 'ヴ' => 'vu', 'ヴェ' => 've', 'ヴォ' => 'vo',
+        'ヴャ' => 'vya', 'ヴュ' => 'vyu', 'ヴィェ' => 'vye', 'ヴョ' => 'vyo',
+        'キェ' => 'kye',
+        'ギェ' => 'gye',
+        'クァ' => 'kwa', 'クィ' => 'kwi', 'クェ' => 'kwe', 'クォ' => 'kwo',
+        'クヮ' => 'kwa',
+        'グァ' => 'gwa', 'グィ' => 'gwi', 'グェ' => 'gwe', 'グォ' => 'gwo',
+        'グヮ' => 'gwa',
+        'シェ' => 'she',
+        'ジェ' => 'je',
+        'スィ' => 'si',
+        'ズィ' => 'zi',
+        'チェ' => 'che',
+        'ツァ' => 'tsa', 'ツィ' => 'tsi', 'ツェ' => 'tse', 'ツォ' => 'tso',
+        'ツュ' => 'tsyu',
+        'ティ' => 'ti', 'テゥ' => 'tu',
+        'テュ' => 'tyu',
+        'ディ' => 'di', 'デゥ' => 'du',
+        'デュ' => 'dyu',
+        'ニェ' => 'nye',
+        'ヒェ' => 'hye',
+        'ビェ' => 'bye',
+        'ピェ' => 'pye',
+        'ファ' => 'fa', 'フィ' => 'fi', 'フェ' => 'fe', 'フォ' => 'fo',
+        'フャ' => 'fya', 'フュ' => 'fyu', 'フィェ' => 'fye', 'フョ' => 'fyo',
+        'ホゥ' => 'hu',
+        'ミェ' => 'mye',
+        'リェ' => 'rye',
+        'ラ゜' => 'la', 'リ゜' => 'li', 'ル゜' => 'lu', 'レ゜' => 'le', 'ロ゜' => 'lo',
+        'ヷ' => 'va', 'ヸ' => 'vi', 'ヹ' => 've', 'ヺ' => 'vo',
+        'ンア' => "n'a", 'ンイ' => "n'i", 'ンウ' => "n'u", 'ンエ' => "n'e", 'ンオ' => "n'o",
+        'ンヤ' => "n'ya", 'ンユ' => "n'yu", 'ンヨ' => "n'yo",
+    );
+
+    /**
+     * @var array Map Japanese punctuation marks to their equivalent in latin
+     * alphabet.
+     *
+     * @see http://en.wikipedia.org/wiki/Japanese_punctuation
+     */
+    protected $mapPunctuationMarks = array(
+        '　' => ' ', '、' => ', ', '，' => ', ', '：' => ':', '・' => '-',
+        '。' => '.', '！' => '!', '？' => '?', '‥' => '…',
+        '「' => '\'', '」' => '\'', '『' => '"', '』' => '"',
+        '（' => '(', '）' => ')', '｛' => '{', '｝' => '}',
+        '［' => '[', '］' => ']', '【' => '[', '】' => ']',
+        '〜' => '~', '〽' => '\'',
+    );
+
 
     /**
      * Escapes latin characters [a-z].
      */
     protected function escapeLatinCharacters($str)
     {
-        $obj = $this;
-        $str = preg_replace_callback(Romanization::PREG_PATTERN_ESCAPE_CHAR, function($matches) use ($obj) {
-            $obj->latinCharacters[] = $matches[1];
-
-            return '%s';
-        }, $str);
-
+        $str = preg_replace_callback(Romanization::PREG_PATTERN_ESCAPE_CHAR, array($this, "espaceLatinCharactersCallback"), $str);
         return $str;
+    }
+
+    /**
+     * Private callback for escapeLatinCharacters().
+     */
+    private function espaceLatinCharactersCallback($matches)
+    {
+        $this->latinCharacters[] = $matches[1];
+        return '%s';
     }
 
     /**
