f2a879b6ebdf1c79ad9f189edaf51b6264815951
Matthieu Bilbille <matthieu.bilbille@gmail.com>
2015-06-18 12:53:34 +0000

    Add support for Hepburn traditional system FIX #43

--- src/JpnForPhp/Transliterator/Romaji.php
+++ src/JpnForPhp/Transliterator/Romaji.php
@@ -147,6 +147,18 @@ class Romaji extends TransliterationSystem
     {
         return str_replace(array_keys($parameters['particules']), array_values($parameters['particules']), $str);
     }
+    
+    /**
+     * Transliterate character 'n' to 'm' before labial consonants.
+     *
+     * @param string $str        String to be transliterated.
+     * 
+     * @return string Transliterated string.
+     */
+    protected function transliterateNBeforeLabialConsonants($str)
+    {
+        return preg_replace('/n([bmp])/u', 'm$1', $str);
+    }
 
     /**
      * Escapes latin characters [a-z].
