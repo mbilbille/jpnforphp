a9546495d13a7fe6fa81df293506c24648164942
Akeru <axel.bodart@gmail.com>
2015-08-03 15:51:21 +0200

    Add initial support for year conversion

--- tests/JpnForPhp/Converter/ConverterTest.php
+++ tests/JpnForPhp/Converter/ConverterTest.php
@@ -28,31 +28,31 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
         $result = Converter::toJapaneseNumeral(2);
         $this->assertEquals('二', $result);
     }
-    
+
     public function testConversionToJapaneseNumeralWithNumberSupToTen()
     {
         $result = Converter::toJapaneseNumeral(13);
         $this->assertEquals('十三', $result);
     }
-    
+
     public function testConversionToJapaneseNumeralWithNumberSupToHundred()
     {
         $result = Converter::toJapaneseNumeral(528);
         $this->assertEquals('五百二十八', $result);
-    }  
-    
+    }
+
     public function testConversionToJapaneseNumeralWithNumberSupToThousand()
     {
         $result = Converter::toJapaneseNumeral(2444);
         $this->assertEquals('二千四百四十四', $result);
     }
-    
+
     public function testConversionToJapaneseNumeralWithNumberSupToTenThousand()
     {
         $result = Converter::toJapaneseNumeral(45287);
         $this->assertEquals('四万五千二百八十七', $result);
     }
-    
+
     public function testConversionToJapaneseNumeralWithNumberSupToHundredMillion()
     {
         $result = Converter::toJapaneseNumeral(123456789);
@@ -131,4 +131,124 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('ichō go hyaku kyū oku issen kyū hyaku kyū man ni sen kyū hyaku hachi jū go', $result);
     }
 
+    public function testConversionToJapaneseYearDefault615()
+    {
+        $result = Converter::toJapaneseYear(645);
+        $this->assertEquals('大化1年', $result);
+    }
+
+    public function testConversionToJapaneseYearDefault1900()
+    {
+        $result = Converter::toJapaneseYear(1900);
+        $this->assertEquals('明治33年', $result);
+    }
+
+    public function testConversionToJapaneseYearKanji1900()
+    {
+        $result = Converter::toJapaneseYear(1900, Converter::YEAR_KANJI);
+        $this->assertEquals('明治33年', $result);
+    }
+
+    public function testConversionToJapaneseYearKanjiNumeralKanji1900()
+    {
+        $result = Converter::toJapaneseYear(1900, Converter::YEAR_KANJI, Converter::NUMERAL_KANJI);
+        $this->assertEquals('明治三十三年', $result);
+    }
+
+    public function testConversionToJapaneseYearRomaji1900()
+    {
+        $result = Converter::toJapaneseYear(1900, Converter::YEAR_ROMAJI);
+        $this->assertEquals('Meiji 33', $result);
+    }
+
+    public function testConversionToJapaneseYearKana1900()
+    {
+        $result = Converter::toJapaneseYear(1900, Converter::YEAR_KANA);
+        $this->assertEquals('めいじ33ねん', $result);
+    }
+
+    public function testConversionToJapaneseYearDefault600()
+    {
+        $this->setExpectedException('Exception');
+        Converter::toJapaneseYear(600);
+        $this->setExpectedException(null);
+    }
+
+    public function testConversionToJapaneseYearDefault2015()
+    {
+        $result = Converter::toJapaneseYear(2015);
+        $this->assertEquals('平成27年', $result);
+    }
+
+    public function testConversionToJapaneseYearDefault2018()
+    {
+        $result = Converter::toJapaneseYear(2018);
+        $this->assertEquals('平成30年', $result);
+    }
+
+    public function testConversionToJapaneseYearRomaji1336()
+    {
+        $result = Converter::toJapaneseYear(1336, Converter::YEAR_ROMAJI);
+        $this->assertEquals('Shōkei 5', $result);
+    }
+
+    public function testConversionToWesternYearRomajiMeiji33()
+    {
+        $result = Converter::toWesternYear('Meiji 33');
+        $this->assertEquals('1900', $result);
+    }
+
+    public function testConversionToWesternYearKanjiMeiji33()
+    {
+        $result = Converter::toWesternYear('明治33');
+        $this->assertEquals('1900', $result);
+    }
+
+    public function testConversionToWesternYearKanaMeiji33()
+    {
+        $result = Converter::toWesternYear('めいじ33');
+        $this->assertEquals('1900', $result);
+    }
+
+    public function testConversionToWesternYearRomajiShowa5()
+    {
+        $results = Converter::toWesternYear('Shōwa 5');
+        $this->assertInternalType('array', $results);
+        $this->assertEquals(2, count($results));
+        $this->assertEquals('1316', $results[0]);
+        $this->assertEquals('1930', $results[1]);
+    }
+
+    public function testConversionToWesternYearRomajiShowa9()
+    {
+        $results = Converter::toWesternYear('Shōwa 9');
+        $this->assertEquals('1934', $results);
+    }
+
+    public function testConversionToWesternYearKanaMeiji46()
+    {
+        $this->setExpectedException('Exception');
+        Converter::toWesternYear('めいじ46');
+        $this->setExpectedException(null);
+    }
+
+    public function testConversionToWesternYearKanaMeiji45()
+    {
+        $result = Converter::toWesternYear('めいじ45');
+        $this->assertEquals('1912', $result);
+    }
+
+    public function testConversionToWesternYearKanaMeiji0()
+    {
+        $this->setExpectedException('Exception');
+        Converter::toWesternYear('めいじ0');
+        $this->setExpectedException(null);
+    }
+
+    public function testConversionToWesternYearKanaMeji10()
+    {
+        $this->setExpectedException('Exception');
+        Converter::toWesternYear('めじ10');
+        $this->setExpectedException(null);
+    }
 }
