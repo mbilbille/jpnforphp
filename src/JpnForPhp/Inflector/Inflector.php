c4a794d109db029d37cebd173ff7acfbe040624b
Akeru <axel.bodart@gmail.com>
2015-08-07 20:15:47 +0200

    Add support for inflection form selection

--- src/JpnForPhp/Inflector/Inflector.php
+++ src/JpnForPhp/Inflector/Inflector.php
@@ -20,7 +20,7 @@ use PDO;
 /**
  * Provides support for inflecting Japanese verbs.
  *
- * @author Axel Bodart (@akeru)
+ * @author Axel Bodart (@akeru) based on work by Fabien SK (@fabiensk)
  */
 class Inflector
 {
@@ -67,6 +67,51 @@ class Inflector
     const LOOK_LIKE_POLITE = "LOOK_LIKE_POLITE";
     const NOT_LOOK_LIKE_NEUTRAL = "NOT_LOOK_LIKE_NEUTRAL";
     const NOT_LOOK_LIKE_POLITE = "NOT_LOOK_LIKE_POLITE";
+
+    private static $ALL = array(
+        self::NON_PAST_POLITE,
+        self::NON_PAST_NEGATIVE,
+        self::NON_PAST_NEGATIVE_POLITE,
+        self::PAST,
+        self::PAST_POLITE,
+        self::PAST_NEGATIVE,
+        self::PAST_NEGATIVE_POLITE,
+        self::BASE_CONNECTIVE,
+        self::SUSPENSIVE_FORM,
+        self::PROGRESSIVE,
+        self::PROGRESSIVE_POLITE,
+        self::PROGRESSIVE_NEGATIVE,
+        self::PROGRESSIVE_NEGATIVE_POLITE,
+        self::PASSIVE,
+        self::PASSIVE_SUSPENSIVE,
+        self::IMPERATIVE_NEUTRAL,
+        self::IMPERATIVE_POLITE,
+        self::IMPERATIVE_POLITE_NEGATIVE,
+        self::IMPERATIVE_HARD,
+        self::OPTATIVE,
+        self::OPTATIVE_NEGATIVE_FAMILIAR,
+        self::OPTATIVE_NEGATIVE_POLITE_1,
+        self::OPTATIVE_NEGATIVE_POLITE_2,
+        self::OPTATIVE_PAST,
+        self::OPTATIVE_NEGATIVE,
+        self::OPTATIVE_PAST_NEGATIVE_POLITE_1,
+        self::OPTATIVE_PAST_NEGATIVE_POLITE_2,
+        self::OPTATIVE_SUSPENSIVE,
+        self::OPTATIVE_CONDITIONAL,
+        self::GERUND,
+        self::FACTITIVE,
+        self::FACTITIVE_SHORTENED,
+        self::POTENTIAL_NEUTRAL,
+        self::POTENTIAL_POLITE,
+        self::CONDITIONAL_BA,
+        self::CONDITIONAL_TARA,
+        self::VOLITIONAL_FAMILIAR,
+        self::VOLITIONAL_POLITE,
+        self::LOOK_LIKE_NEUTRAL,
+        self::LOOK_LIKE_POLITE,
+        self::NOT_LOOK_LIKE_NEUTRAL,
+        self::NOT_LOOK_LIKE_POLITE
+    );
 
     /**
      * Generates full mapping for all known verb types
@@ -288,16 +333,157 @@ class Inflector
     }
 
     /**
-     * Generates conjugation for the given verb to the given type using the given suffix
+     * Generates conjugation for the given verb to the given type using the given mappings
      *
      * @param array $verb
-     * @param $suffix
-     * @param $type
+     * @param array $mappings
+     * @param $form
      * @return array
+     * @throws Exception
      */
-    private static function conjugateEntry(array $verb, $suffix, $type)
+    private static function inflectForm(array $verb, array $mappings, $form)
     {
-        $radicals = self::getRadicals($verb, $type);
+        $radicals = self::getRadicals($verb, $form);
+        switch ($form) {
+            case self::NON_PAST_POLITE:
+                $suffix = $mappings['polite'];
+                break;
+            case self::NON_PAST_NEGATIVE:
+                $suffix = $mappings['neg'];
+                break;
+            case self::NON_PAST_NEGATIVE_POLITE:
+                $suffix = $mappings['base_i'] . 'ません';
+                break;
+            case self::PAST:
+                $suffix = $mappings['past'];
+                break;
+            case self::PAST_POLITE:
+                $suffix = $mappings['base_i'] . 'ました';
+                break;
+            case self::PAST_NEGATIVE:
+                $suffix = $mappings['neg_past'];
+                break;
+            case self::PAST_NEGATIVE_POLITE:
+                $suffix = $mappings['polite_past_neg'];
+                break;
+            case self::BASE_CONNECTIVE:
+                if (array_key_exists('connective', $mappings)) {
+                    $suffix = $mappings['connective'];
+                } else {
+                    $suffix = $mappings['base_i'];
+                }
+                $suffix .= '-';
+                break;
+            case self::SUSPENSIVE_FORM:
+                $suffix = $mappings['suspensive'];
+                break;
+            case self::PROGRESSIVE:
+                $suffix = $mappings['suspensive'] . 'いる';
+                break;
+            case self::PROGRESSIVE_POLITE:
+                $suffix = $mappings['suspensive'] . 'います';
+                break;
+            case self::PROGRESSIVE_NEGATIVE:
+                $suffix = $mappings['suspensive'] . 'ない';
+                break;
+            case self::PROGRESSIVE_NEGATIVE_POLITE:
+                $suffix = $mappings['suspensive'] . 'いません';
+                break;
+            case self::PASSIVE:
+                $suffix = $mappings['base_passive'] . 'れる';
+                break;
+            case self::PASSIVE_SUSPENSIVE:
+                $suffix = $mappings['base_passive'] . 'れて';
+                break;
+            case self::IMPERATIVE_NEUTRAL:
+                $suffix = $mappings['imperative_neutral'];
+                break;
+            case self::IMPERATIVE_POLITE:
+                $suffix = $mappings['suspensive'] . 'ください';
+                break;
+            case self::IMPERATIVE_POLITE_NEGATIVE:
+                $suffix = $mappings['neg'] . 'でください';
+                break;
+            case self::IMPERATIVE_HARD:
+                $suffix = $mappings['imper_hard'];
+                break;
+            case self::OPTATIVE:
+                $suffix = $mappings['base_i'] . 'たい';
+                break;
+            case self::OPTATIVE_NEGATIVE_FAMILIAR:
+                $suffix = $mappings['base_i'] . 'たくない';
+                break;
+            case self::OPTATIVE_NEGATIVE_POLITE_1:
+                $suffix = $mappings['base_i'] . 'たくないです';
+                break;
+            case self::OPTATIVE_NEGATIVE_POLITE_2:
+                $suffix = $mappings['base_i'] . 'たくありません';
+                break;
+            case self::OPTATIVE_PAST:
+                $suffix = $mappings['base_i'] . 'たかった';
+                break;
+            case self::OPTATIVE_NEGATIVE:
+                $suffix = $mappings['base_i'] . 'たくなかった';
+                break;
+            case self::OPTATIVE_PAST_NEGATIVE_POLITE_1:
+                $suffix = $mappings['base_i'] . 'たくなかったです';
+                break;
+            case self::OPTATIVE_PAST_NEGATIVE_POLITE_2:
+                $suffix = $mappings['base_i'] . 'たくありませんでした';
+                break;
+            case self::OPTATIVE_SUSPENSIVE:
+                $suffix = $mappings['base_i'] . 'たくて';
+                break;
+            case self::OPTATIVE_CONDITIONAL:
+                $suffix = $mappings['base_i'] . 'たければ';
+                break;
+            case self::GERUND:
+                $suffix = $mappings['gerund'];
+                break;
+            case self::FACTITIVE:
+                $suffix = $mappings['factitive'];
+                break;
+            case self::FACTITIVE_SHORTENED:
+                $suffix = $mappings['factitive_c'];
+                break;
+            case self::POTENTIAL_NEUTRAL:
+                $suffix = $mappings['base_e'] . 'る';
+                break;
+            case self::POTENTIAL_POLITE:
+                $suffix = $mappings['base_e'] . 'ます';
+                break;
+            case self::CONDITIONAL_BA:
+                if (array_key_exists('cond', $mappings)) {
+                    $suffix = $mappings['cond'];
+                } else {
+                    $suffix = $mappings['base_e'];
+                }
+                $suffix .= 'ば';
+                break;
+            case self::CONDITIONAL_TARA:
+                $suffix = $mappings['past'] . 'ら';
+                break;
+            case self::VOLITIONAL_FAMILIAR:
+                $suffix = $mappings['volition'];
+                break;
+            case self::VOLITIONAL_POLITE:
+                $suffix = $mappings['volition_polite'];
+                break;
+            case self::LOOK_LIKE_NEUTRAL:
+                $suffix = $mappings['base_i'] . 'そうだ';
+                break;
+            case self::LOOK_LIKE_POLITE:
+                $suffix = $mappings['base_i'] . "そうです";
+                break;
+            case self::NOT_LOOK_LIKE_NEUTRAL:
+                $suffix = $mappings['base_neg'] . "なさそうだ";
+                break;
+            case self::NOT_LOOK_LIKE_POLITE:
+                $suffix = $mappings['base_neg'] . "なさそうです";
+                break;
+            default:
+                throw new Exception('Unknown form ' . $form);
+        }
         $kanjiRadical = $radicals['kanji'];
         if (!empty($kanjiRadical)) {
             $kanjiValue = $kanjiRadical . $suffix;
@@ -311,7 +497,7 @@ class Inflector
     /**
      * Helper method the generate the database. The JMDict file my be downloaded separately
      */
-    public static function generate()
+    public static function createDatabase()
     {
         $ddl = file_get_contents('verbs.sql');
         if (file_exists('verbs.db')) {
@@ -369,111 +555,29 @@ class Inflector
     }
 
     /**
-     * Conjugates the verb to all known forms
+     * Inflects the verb to given forms or all if none input
      *
      * @param $verb
+     * @param array $forms
      * @return array
      * @throws Exception
      */
-    public static function conjugate($verb)
+    public static function inflect($verb, $forms = array())
     {
         $ret = array();
         if (!empty($verb)) {
             $type = $verb['type'];
             $mapVerbs = self::makeVerbMappings();
             if (array_key_exists($type, $mapVerbs)) {
-                $entries = $mapVerbs[$type];
-                $past = $entries['past'];
-                $suspensive = $entries['suspensive'];
-                $base_i = $entries['base_i'];
-                $imperative_neutral = $entries['imperative_neutral'];
-                $gerund = $entries['gerund'];
-                $polite = $entries['polite'];
-                $volition_polite = $entries['volition_polite'];
-                $polite_past_neg = $entries['polite_past_neg'];
-                $neg = $entries['neg'];
-                $base_neg = $entries['base_neg'];
-                $neg_past = $entries['neg_past'];
-                $factitive = $entries['factitive'];
-                $factitive_c = $entries['factitive_c'];
-                $base_passive = $entries['base_passive'];
-                $imper_hard = $entries['imper_hard'];
-                $base_e = $entries['base_e'];
-                $volition = $entries['volition'];
-                $potential = $base_e . 'る';
-                $potential_polite = $base_e . 'ます';
-                $polite_past = $base_i . 'ました';
-                $neg_polite = $base_i . 'ません';
-                $optative = $base_i . 'たい';
-                $optative_cond = $base_i . 'たければ';
-                $optative_neg = $base_i . 'たくない';
-                $optative_neg_polite_1 = $base_i . 'たくないです';
-                $optative_neg_polite_2 = $base_i . 'たくありません';
-                $optative_past = $base_i . 'たかった';
-                $optative_past_neg = $base_i . 'たくなかった';
-                $optative_past_neg_polite_1 = $base_i . 'たくなかったです';
-                $optative_past_neg_polite_2 = $base_i . 'たくありませんでした';
-                $optative_suspensive = $base_i . 'たくて';
-                $passive = $base_passive . 'れる';
-                $passive_suspensive = $base_passive . 'れて';
-                if (array_key_exists('cond', $entries)) {
-                    $cond = $entries['cond'];
-                } else {
-                    $cond = $base_e;
+                $mappings = $mapVerbs[$type];
+                if (is_string($forms)) {
+                    return self::inflectForm($verb, $mappings, $forms);
+                } elseif (empty($forms)) {
+                    $forms = self::$ALL;
                 }
-                if (array_key_exists('connective', $entries)) {
-                    $connective = $entries['connective'];
-                } else {
-                    $connective = $base_i;
+                foreach ($forms as $form) {
+                    $ret[$form] = self::inflectForm($verb, $mappings, $form);
                 }
-                $cond .= 'ば';
-                $cond_tara = $past . 'ら';
-                $look_like = $base_i . 'そうだ';
-                $look_like_polite = $base_i . "そうです";
-                $not_look_like = $base_neg . "なさそうだ";
-                $not_look_like_polite = $base_neg . "なさそうです";
-                $ret[self::NON_PAST_POLITE] = self::conjugateEntry($verb, $polite, self::NON_PAST_POLITE);
-                $ret[self::NON_PAST_NEGATIVE] = self::conjugateEntry($verb, $neg, self::NON_PAST_NEGATIVE);
-                $ret[self::NON_PAST_NEGATIVE_POLITE] = self::conjugateEntry($verb, $neg_polite, self::NON_PAST_NEGATIVE_POLITE);
-                $ret[self::PAST] = self::conjugateEntry($verb, $past, self::PAST);
-                $ret[self::PAST_POLITE] = self::conjugateEntry($verb, $polite_past, self::PAST_POLITE);
-                $ret[self::PAST_NEGATIVE] = self::conjugateEntry($verb, $neg_past, self::PAST_NEGATIVE);
-                $ret[self::PAST_NEGATIVE_POLITE] = self::conjugateEntry($verb, $polite_past_neg, self::PAST_NEGATIVE_POLITE);
-                $ret[self::BASE_CONNECTIVE] = self::conjugateEntry($verb, $connective . '-', self::BASE_CONNECTIVE);
-                $ret[self::SUSPENSIVE_FORM] = self::conjugateEntry($verb, $suspensive, self::SUSPENSIVE_FORM);
-                $ret[self::PROGRESSIVE] = self::conjugateEntry($verb, $suspensive . 'いる', self::PROGRESSIVE);
-                $ret[self::PROGRESSIVE_POLITE] = self::conjugateEntry($verb, $suspensive . 'います', self::PROGRESSIVE_POLITE);
-                $ret[self::PROGRESSIVE_NEGATIVE] = self::conjugateEntry($verb, $suspensive . 'ない', self::PROGRESSIVE_NEGATIVE);
-                $ret[self::PROGRESSIVE_NEGATIVE_POLITE] = self::conjugateEntry($verb, $suspensive . 'いません', self::PROGRESSIVE_NEGATIVE_POLITE);
-                $ret[self::PASSIVE] = self::conjugateEntry($verb, $passive, self::PASSIVE);
-                $ret[self::PASSIVE_SUSPENSIVE] = self::conjugateEntry($verb, $passive_suspensive, self::PASSIVE_SUSPENSIVE);
-                $ret[self::IMPERATIVE_NEUTRAL] = self::conjugateEntry($verb, $imperative_neutral, self::IMPERATIVE_NEUTRAL);
-                $ret[self::IMPERATIVE_POLITE] = self::conjugateEntry($verb, $suspensive . 'ください', self::IMPERATIVE_POLITE);
-                $ret[self::IMPERATIVE_POLITE_NEGATIVE] = self::conjugateEntry($verb, $neg . 'でください', self::IMPERATIVE_POLITE_NEGATIVE);
-                $ret[self::IMPERATIVE_HARD] = self::conjugateEntry($verb, $imper_hard, self::IMPERATIVE_HARD);
-                $ret[self::OPTATIVE] = self::conjugateEntry($verb, $optative, self::OPTATIVE);
-                $ret[self::OPTATIVE_NEGATIVE_FAMILIAR] = self::conjugateEntry($verb, $optative_neg, self::OPTATIVE_NEGATIVE_FAMILIAR);
-                $ret[self::OPTATIVE_NEGATIVE_POLITE_1] = self::conjugateEntry($verb, $optative_neg_polite_1, self::OPTATIVE_NEGATIVE_POLITE_1);
-                $ret[self::OPTATIVE_NEGATIVE_POLITE_2] = self::conjugateEntry($verb, $optative_neg_polite_2, self::OPTATIVE_NEGATIVE_POLITE_2);
-                $ret[self::OPTATIVE_PAST] = self::conjugateEntry($verb, $optative_past, self::OPTATIVE_PAST);
-                $ret[self::OPTATIVE_NEGATIVE] = self::conjugateEntry($verb, $optative_past_neg, self::OPTATIVE_NEGATIVE);
-                $ret[self::OPTATIVE_PAST_NEGATIVE_POLITE_1] = self::conjugateEntry($verb, $optative_past_neg_polite_1, self::OPTATIVE_PAST_NEGATIVE_POLITE_1);
-                $ret[self::OPTATIVE_PAST_NEGATIVE_POLITE_2] = self::conjugateEntry($verb, $optative_past_neg_polite_2, self::OPTATIVE_PAST_NEGATIVE_POLITE_2);
-                $ret[self::OPTATIVE_SUSPENSIVE] = self::conjugateEntry($verb, $optative_suspensive, self::OPTATIVE_SUSPENSIVE);
-                $ret[self::OPTATIVE_CONDITIONAL] = self::conjugateEntry($verb, $optative_cond, self::OPTATIVE_CONDITIONAL);
-                $ret[self::GERUND] = self::conjugateEntry($verb, $gerund, self::GERUND);
-                $ret[self::FACTITIVE] = self::conjugateEntry($verb, $factitive, self::FACTITIVE);
-                $ret[self::FACTITIVE_SHORTENED] = self::conjugateEntry($verb, $factitive_c, self::FACTITIVE_SHORTENED);
-                $ret[self::POTENTIAL_NEUTRAL] = self::conjugateEntry($verb, $potential, self::POTENTIAL_NEUTRAL);
-                $ret[self::POTENTIAL_POLITE] = self::conjugateEntry($verb, $potential_polite, self::POTENTIAL_POLITE);
-                $ret[self::CONDITIONAL_BA] = self::conjugateEntry($verb, $cond, self::CONDITIONAL_BA);
-                $ret[self::CONDITIONAL_TARA] = self::conjugateEntry($verb, $cond_tara, self::CONDITIONAL_TARA);
-                $ret[self::VOLITIONAL_FAMILIAR] = self::conjugateEntry($verb, $volition, self::VOLITIONAL_FAMILIAR);
-                $ret[self::VOLITIONAL_POLITE] = self::conjugateEntry($verb, $volition_polite, self::VOLITIONAL_POLITE);
-                $ret[self::LOOK_LIKE_NEUTRAL] = self::conjugateEntry($verb, $look_like, self::LOOK_LIKE_NEUTRAL);
-                $ret[self::LOOK_LIKE_POLITE] = self::conjugateEntry($verb, $look_like_polite, self::LOOK_LIKE_POLITE);
-                $ret[self::NOT_LOOK_LIKE_NEUTRAL] = self::conjugateEntry($verb, $not_look_like, self::NOT_LOOK_LIKE_NEUTRAL);
-                $ret[self::NOT_LOOK_LIKE_POLITE] = self::conjugateEntry($verb, $not_look_like_polite, self::NOT_LOOK_LIKE_POLITE);
             } else {
                 throw new Exception("Unknown verb type : " . $type);
             }
@@ -481,6 +585,3 @@ class Inflector
         return $ret;
     }
 }
-
-//Inflector::generate();
-
