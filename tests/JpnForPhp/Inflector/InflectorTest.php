f144d1faa4495153ebd25411003295f097d9620d
Matthieu Bilbille <matthieu.bilbille@gmail.com>
2017-07-18 15:13:19 +0200

    Plug vs-i and vs-s tests To support suru verb - special class and suru verb - irregular

--- tests/JpnForPhp/Inflector/InflectorTest.php
+++ tests/JpnForPhp/Inflector/InflectorTest.php
@@ -54,80 +54,84 @@ class InflectorTest extends \PHPUnit_Framework_TestCase
         $this->assertNotEmpty(InflectorUtils::getEntriesFromDatabase('miru'));
     }
 
-    public function testInflect1()
+    public function testInflect_v1()
     {
         $this->assertInflectionFromFile('見る', 'miru.yml');
         $this->assertInflectionFromFile('食べる', 'taberu.yml');
     }
 
-    public function testInflect5k()
+    public function testInflect_v5k()
     {
         $this->assertInflectionFromFile('焼く', 'yaku.yml');
     }
 
-    public function testInflect5ks()
+    public function testInflect_v5ks()
     {
         $this->assertInflectionFromFile('行く', 'iku.yml');
     }
 
-    public function testInflect5g()
+    public function testInflect_v5g()
     {
         $this->assertInflectionFromFile('泳ぐ', 'oyogu.yml');
     }
 
-    public function testInflect5s()
+    public function testInflect_v5s()
     {
         $this->assertInflectionFromFile('放す', 'hanasu.yml');
     }
 
-    public function testInflect5t()
+    public function testInflect_v5t()
     {
         $this->assertInflectionFromFile('待つ', 'matsu.yml');
     }
 
-    public function testInflect5n()
+    public function testInflect_v5n()
     {
         $this->assertInflectionFromFile('死ぬ', 'shinu.yml');
     }
 
-    public function testInflect5b()
+    public function testInflect_v5b()
     {
         $this->assertInflectionFromFile('呼ぶ', 'yobu.yml');
     }
 
-    public function testInflect5m()
+    public function testInflect_v5m()
     {
       $this->assertInflectionFromFile('読む', 'yomu.yml');
     }
 
-    public function testInflect5r()
+    public function testInflect_v5r()
     {
       $this->assertInflectionFromFile('走る', 'hashiru.yml');
     }
 
-    public function testInflect5aru()
+    public function testInflect_v5aru()
     {
       $this->assertInflectionFromFile('いらっしゃる', 'irassharu.yml');
     }
 
-    public function testInflect5ri()
+    public function testInflect_v5ri()
     {
       $this->assertInflectionFromFile('有る', 'aru.yml');
     }
 
-    public function testInflect5u()
+    public function testInflect_v5u()
     {
       $this->assertInflectionFromFile('使う', 'tsukau.yml');
     }
 
-    public function testInflect5us()
+    public function testInflect_v5us()
     {
       $this->assertInflectionFromFile('請う', 'kou.yml');
     }
 
-    public function testInflectk()
+    public function testInflect_vs()
     {
-      $this->assertInflectionFromFile('来る', 'kuru.yml');
-      $this->assertInflectionFromFile('持ってくる', 'mottekuru.yml');
+      $this->assertInflectionFromFile('する', 'suru.yml');
+    }
+
+    public function testInflect_vss()
+    {
+      $this->assertInflectionFromFile('愛する', 'aisuru.yml');
     }
 }
